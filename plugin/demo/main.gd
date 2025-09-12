a6637805c7918bb8e89c3a94cd5ffa405c1923e1
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-09-12 19:14:23 -0300

    updating demo

--- plugin/demo/main.gd
+++ plugin/demo/main.gd
@@ -1,14 +1,23 @@
 extends Control
 
+@export var icon: Texture2D
 @onready var scroll: ScrollContainer = $ScrollContainer
 
 var _plugin_name = "NotificationHandler"
 var _channel_id = "news_channel"
+var _small_icon = "small_icon.png"
 var _android_plugin
 
 func _ready():
 	call_deferred("_init_sizes")
 	self.plugin_exists()
+	
+	# Save image in user.
+	if !Engine.is_editor_hint():
+		var small_icon_path = "user://" + _small_icon
+		var text = load(icon.resource_path) as Texture2D
+		var img = text.get_image()
+		img.save_png(small_icon_path)
 
 
 func _init_sizes():
@@ -36,7 +45,7 @@ func _on_build_pressed() -> void:
 			"channelId": _channel_id,
 			"name": "News",
 			"description": "Notifications about news",
-			"importance": 3
+			"importance": 4
 		}
 		var test_array = JSON.stringify([channel_data])
 		_android_plugin.setup(test_array)
@@ -44,9 +53,21 @@ func _on_build_pressed() -> void:
 
 func _on_simple_pressed() -> void:
 	if _android_plugin:
-		var channel_id = _channel_id
 		var title = "Hello"
 		var content = "This is a test notification"
-		var icon_path = "res://icon.svg"
-		
-		_android_plugin.triggerNotification(channel_id, title, content, icon_path)
+		var res = _android_plugin.triggerNotification(
+			_channel_id,
+			title,
+			content,
+			_small_icon
+		)
+		print("Result: ", res)
+
+
+func _on_permissions_pressed() -> void:
+	if _android_plugin:
+		_android_plugin.requestPostNotificationsPermissions()
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
