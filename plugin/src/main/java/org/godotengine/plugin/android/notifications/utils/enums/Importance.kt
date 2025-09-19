1f72b4f70c96c437875d2e6343f6169d8b7bd75a
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-09-18 21:29:21 -0300

    fix: missing min

--- plugin/src/main/java/org/godotengine/plugin/android/notifications/utils/enums/Importance.kt
+++ plugin/src/main/java/org/godotengine/plugin/android/notifications/utils/enums/Importance.kt
@@ -4,6 +4,7 @@ import android.app.NotificationManager
 
 enum class Importance(val value: Int) {
     NONE(NotificationManager.IMPORTANCE_NONE),
+    MIN(NotificationManager.IMPORTANCE_MIN),
     LOW(NotificationManager.IMPORTANCE_LOW),
     DEFAULT(NotificationManager.IMPORTANCE_DEFAULT),
     HIGH(NotificationManager.IMPORTANCE_HIGH)
